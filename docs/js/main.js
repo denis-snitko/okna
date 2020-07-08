84033a02e463c50c905487af98dbdbf80af9e54b
Denis Snitko <denissnitko@gmail.com>
2020-07-08 15:37:44 +0300

    Update

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,24 @@
 
+let sliders = document.querySelectorAll('.swiper-container');
+
+sliders.forEach((el) => {
+    new Swiper('.swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 5,
+        centeredSlides: true,
+        loop: true,
+        navigation: {
+            prevEl: el.querySelector('.swiper-button-prev'),
+            nextEl: el.querySelector('.swiper-button-next')
+        },
+        
+        breakpoints: {
+            548: {
+                slidesPerView: 6,
+                spaceBetween: 5,
+                centeredSlides: false,
+            },
+        }
+    });
+
+})
