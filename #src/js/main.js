28c721e471a3992296d0814a5237db3c2b0cee84
Denis Snitko <denissnitko@gmail.com>
2020-07-27 15:55:39 +0300

    Правки

--- #src/js/main.js
+++ #src/js/main.js
@@ -60,7 +60,6 @@ jQuery(function ($) {
     let $showMore = $('.main-item__show-more');
 
     $($bodyLinks).each(function () {
-console.log($(this).height());
         if ($(this).height() >= 56) {
             $(this).attr('style', 'height: 56px');
             $(this).next($showMore).show();
